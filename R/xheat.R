4588d719c8a343338c6cb7c9c7ea7b5b207c3cc9
Ryan Burge <ryanburge@gmail.com>
2019-02-04 18:22:17 -0600

    adding a count option.

--- R/xheat.R
+++ R/xheat.R
@@ -5,30 +5,48 @@
 #' @param df Name of the Dataset
 #' @param var1 This will be the variable that you do group_by with
 #' @param var2 This is the variable that will be counted
+#' @param count Will add the total count to each square if added
 #' @keywords Crosstab, heatmap
 #' @export
 #' @examples
 #' xheat()
 
 
-xheat <- function(df, var1, var2){
+xheat <- function(df, var1, var2, count = TRUE) {
   var1 <- enquo(var1)
   var2 <- enquo(var2)
   
   
-  df1 <- df %>% 
-    group_by(!! var1) %>% 
-    ct(!! var2)
+  if(missing(count)){
+    df1 <- df %>% 
+      group_by(!! var1) %>% 
+      ct(!! var2)
+    
+    df1 %>% 
+      ggplot(., aes(x = !! var1, y = !! var2)) +
+      geom_tile(aes(fill = pct), color = "black") +
+      geom_text(aes(x= !! var1, y = !! var2 + .1, label = paste0(pct*100, '%'))) +
+      scale_fill_gradient(low = "#556270", high = "#FF6B6B")+ 
+      theme_minimal() +
+      theme(legend.position = "none") 
+    
+  } else {
+    
+    df1 <- df %>% 
+      group_by(!! var1) %>% 
+      ct(!! var2)
+    
+    df1 %>% 
+      ggplot(., aes(x = !! var1, y = !! var2)) +
+      geom_tile(aes(fill = pct), color = "black") +
+      geom_text(aes(x= !! var1, y = !! var2 + .1, label = paste0(pct*100, '%'))) +
+      geom_text(aes(x= !! var1, y = !! var2 - .1, label = paste0(n))) +
+      scale_fill_gradient(low = "#556270", high = "#FF6B6B")+ 
+      theme_minimal() +
+      theme(legend.position = "none") 
+    
+  }
   
-   df1 %>% 
-     ggplot(., aes(x = !! var1, y = !! var2)) +
-     geom_tile(aes(fill = pct), color = "black") +
-     geom_text(aes(x= !! var1, y = !! var2, label = paste0(pct*100, '%'))) +
-     scale_fill_gradient(low = "#556270", high = "#FF6B6B")+ 
-     theme_minimal() +
-     theme(legend.position = "none")
-   
-
 }
 
  
