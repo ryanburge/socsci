1bf9578f99c4844c74c2d5251880edccffa890d1
Ryan Burge <ryanburge@gmail.com>
2019-06-21 15:47:11 -0500

    Added the ability to remove NAs before the calculation is made in ct function. It's off by default. show_na = FALSE.

--- R/ct.R
+++ R/ct.R
@@ -5,6 +5,7 @@
 #' @param df Name of the Dataset
 #' @param var Variable to Count
 #' @param wt Weighting Variable
+#' @param show_na Will remove the NAs before the calculation if set to TRUE, defaults to FALSE 
 #' @param cum Will add a cumulative total if set to TRUE
 #' @keywords Count
 #' @export
@@ -12,17 +13,30 @@
 #' ct()
 
 
-ct <- function(df, var, wt, cum = FALSE) {
+
+ct <- function(df, var, wt, cum = FALSE, show_na = TRUE) {
+  
   var <- enquo(var)
   wt <- enquo(wt)
   
-  if(quo_is_missing(wt)) {
+  if(quo_is_missing(wt) && show_na == FALSE) {
     df1 <- df %>%
+      filter(!! var != "NA") %>% 
       count(!! var) %>% 
       mutate(pct = prop.table(n)) %>% 
       mutate(pct = round(pct, 3)) 
     
-  } else {
+  } else if(show_na == FALSE){
+    
+    df1 <- df %>%
+      filter(!! var != "NA") %>% 
+      count(!! var, wt = !! wt) %>% 
+      mutate(pct = prop.table(n)) %>% 
+      mutate(pct = round(pct, 3)) 
+    
+  }
+  
+  else {
     
     df1 <- df %>%
       count(!! var, wt = !! wt) %>% 
@@ -34,10 +48,15 @@ ct <- function(df, var, wt, cum = FALSE) {
   if(cum == TRUE){
     
     df1 %>% 
-      mutate(cum = cumsum(n))
+      mutate(cum_n = cumsum(n)) %>% 
+      mutate(cum_pct = cumsum(pct))
   } else {
     
     df1
+    
+    
+    
+    
   }
 }
 
